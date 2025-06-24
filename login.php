82f4d1c4ed171aa17032ccff16b7e5edf0dcc253
Civer Yoshioka UPP <muarifin@pens.ac.id>
2025-06-24 10:56:52 +0700

    Perbaikan Judul Halamn Login

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
     <title>Login</title>
 </head>
 <body>
-    <h2>Login Page</h2>
+    <h2>Halaman Login</h2>
     <form action="process_login.php" method="post">
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" required>
